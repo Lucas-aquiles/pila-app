044d649cfe998d0df3d794a0b381d9f3404f54c5
Lucas-aquiles <lucasechegaray2011@gmail.com>
2023-08-21 12:48:45 -0300

    second commit

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-class Pila {
+
+export class Pila {
     constructor() {
-      this.items = []; // Usamos un arreglo para almacenar los elementos de la pila
+      this.items = []; 
+      
     }
   
-    // Agregar un elemento a la pila
+    
     push(elemento) {
       this.items.push(elemento);
     }
   
-    // Remover y retornar el último elemento de la pila
+   
     pop() {
       if (this.isEmpty()) {
         return "La pila está vacía";
@@ -16,7 +18,6 @@ class Pila {
       return this.items.pop();
     }
   
-    // Retornar el último elemento de la pila sin removerlo
     peek() {
       if (this.isEmpty()) {
         return "La pila está vacía";
@@ -24,12 +25,10 @@ class Pila {
       return this.items[this.items.length - 1];
     }
   
-    // Verificar si la pila está vacía
     isEmpty() {
       return this.items.length === 0;
     }
   
-    // Retornar el tamaño de la pila
     size() {
       return this.items.length;
     }
@@ -39,24 +38,4 @@ class Pila {
       }
     }
     
-    // Ejemplo de uso de la pila
-    const pila = new Pila();
     
-    console.log(pila.isEmpty()); // true
-    
-    pila.push(10);
-    pila.push(20);
-    pila.push(30);
-    
-    console.log(pila.peek()); // 30
-    console.log(pila.size()); // 3
-    
-    pila.pop();
-    
-    console.log(pila.peek()); // 20
-    
-    console.log(pila.isEmpty()); // false
-    
-    pila.clear();
-    
-    console.log(pila.isEmpty()); // true
